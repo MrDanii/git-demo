c37163be79a2afd947e085cf101ee4430c8d7e67
MrDanii <dan333_d@hotmail.com>
2020-07-29 09:10:44 -0500

    otro print en LoginService

--- controlador/loginService.ts
+++ controlador/loginService.ts
@@ -2,5 +2,6 @@ export class LofinService{
   sendRequest(user: string, pass: string){
     console.log("Doing Somethin >> ", user);
     console.log("Doing Somethin >> ", pass);
+    console.log("Doing Somethin by NikoNikoLay branch>> ", pass);
   }
 }
