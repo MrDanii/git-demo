8058dca9bdd4a9de37dec95643fa2a653258091b
MrDanii <dan333_d@hotmail.com>
2020-07-29 09:12:27 -0500

    otro print en LoginService

--- controlador/loginService.ts
+++ controlador/loginService.ts
@@ -2,5 +2,7 @@ export class LofinService{
   sendRequest(user: string, pass: string){
     console.log("Doing Somethin >> ", user);
     console.log("Doing Somethin >> ", pass);
+    console.log("Print made in master Branch >>");
+    console.log("Print made in master Branch >>");
   }
 }
